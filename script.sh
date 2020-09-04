de11de40b66ff163388f143fa8351e01918269b8
Romain <romain.marcant@gmail.com>
2020-09-04 18:25:26 +0200

    modif script.sh

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ find ./data/sizes -size -3k | wc -l > ./number_of_files_under_3k;
 #Faire un script qui crée un dossier archive_{date} et déplace les documents de plus de 5k
 
 mkdir -p archive_$(date +%d%m%Y);
-find . -path ./archive_$(date +%d%m%Y) -prune -o -type f -size +5k -exec mv {} ./archive_$(date +%d%m%Y) \;
+find . -path ./archive_$(date +%d%m%Y) -prune -o -path ./.git -prune -o -type f -size +5k -exec mv {} ./archive_$(date +%d%m%Y) \;
 
 
 #Faire un script qui donne les fichiers où le nom contient  ta , ecrire le résultat dans un fichier qui s'appelle ta_files
