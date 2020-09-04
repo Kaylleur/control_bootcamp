87dff070d0a956aa7e4ddb0bd3d088567bb9adb7
Maxence <maxence.rouge@ynov.com>
2020-09-04 17:08:00 +0200

    Script Exo 2 et 3 Not Working

--- scripts/ScriptExo2/script_2.sh
+++ scripts/ScriptExo2/script_2.sh
@@ -1,2 +1,3 @@
 #! /usr/bin/bash
+mkdir ~/control_bootcamp/scripts/ScriptExo2/archive_$(date +%d%m%Y)
 find ~/control_bootcamp/data/sizes/ -size +5k -exec mv {} ~/control_bootcamp/scripts/ScriptExo2/archive_$(date +%d%m%Y)/ \;
