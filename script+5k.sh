92279c5f39a84d026c7e9c43fe5e5a0f400068a8
moehjze <moehjze27@gmail.com>
2020-09-04 19:04:26 +0200

    tp_2

--- script+5k.sh
+++ script+5k.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/bash
 
-cd ./data/sizes
+cd ./data
+mkdir archive_$(date +%d%m%Y)
+cd ./sizes
+find . -size +5k -print -exec mv {} ../archive_$(date +%d%m%Y) \;
 
-find . -size +5k -exec cp {} ../archive_$(date +%d%m%Y) \;
